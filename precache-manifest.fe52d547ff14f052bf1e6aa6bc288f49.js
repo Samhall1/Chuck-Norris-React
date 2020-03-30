7818d1429a5732e369952bfebd7d810b08668dfa
Sam Hall <sam_rm@icloud.com>
2020-03-30 18:05:54 +0100

    Updates

---
+++ precache-manifest.fe52d547ff14f052bf1e6aa6bc288f49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2fbd79c6166b82fb90529c5075beda",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "ebb9f96c96f0660555c1",
+    "url": "/react-deploy/static/css/main.b263ede7.chunk.css"
+  },
+  {
+    "revision": "94068279bb592e9c9382",
+    "url": "/react-deploy/static/js/2.eb9e0e04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.eb9e0e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb9f96c96f0660555c1",
+    "url": "/react-deploy/static/js/main.5bd76e67.chunk.js"
+  },
+  {
+    "revision": "e7f915e7e5e5afe53b5c",
+    "url": "/react-deploy/static/js/runtime-main.bcedbe5a.js"
+  },
+  {
+    "revision": "938ca963691cbb836fc6c4f9e923c8a0",
+    "url": "/react-deploy/static/media/chucknorris.938ca963.gif"
+  }
+]);
